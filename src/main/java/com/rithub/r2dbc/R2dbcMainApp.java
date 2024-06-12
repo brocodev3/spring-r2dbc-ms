5cbe5eca2a4a51ff482840b02a0f3e694fefd34e
debakshmi-hub <72191249+rit-hub@users.noreply.github.com>
2024-06-12 21:03:21 +0530

    initial setup

---
+++ src/main/java/com/rithub/r2dbc/R2dbcMainApp.java
@@ -0,0 +1,13 @@
+package com.rithub.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcMainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcMainApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/rithub/r2dbc/R2DbcMainAppTests.java
@@ -0,0 +1,13 @@
+package com.rithub.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2DbcMainAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
